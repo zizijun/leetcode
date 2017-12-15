5a3dab09b4aa5ab6eb0b394d539a2345cee03bf1
金初阳 <2468231855@qq.com>
2017-12-15 17:06:15 +0800

    easy 还是可以做得

---
+++ src/Q121/main.java
@@ -0,0 +1,20 @@
+package Q121;
+
+/**
+ * Created by jinchuyang on 2017/12/15.
+ */
+public class main {
+    public int maxProfit(int[] prices) {
+        if(prices.length == 0){return 0;}
+        int n = prices.length;
+        int profit[] = new int[n];
+        profit[0] = 0;
+        int min = prices[0];
+        for(int i = 1; i < n; i++){
+
+            profit[i] = Math.max(profit[i - 1], prices[i] - min);
+            min = (min > prices[i]) ? prices[i] : min;
+        }
+        return profit[n - 1];
+    }
+}
