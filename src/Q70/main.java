1863797078d7e42f7f1f26be1179677ff4516017
金初阳 <2468231855@qq.com>
2017-12-16 21:00:20 +0800

    今天做了四题

---
+++ src/Q70/main.java
@@ -0,0 +1,19 @@
+package Q70;
+
+/**
+ * Created by jinchuyang on 2017/12/16.
+ */
+public class main {
+    //裴波那切数列
+    public int climbStairs(int n) {
+        if(n <= 0){return 0;}
+        if(n == 1){return 1;}
+        int step[] = new int[n + 1];
+        step[0] = 1;
+        step[1] = 1;
+        for(int i = 2; i <= n; i++){
+            step[i] = step[i - 1] + step[i - 2];
+        }
+        return step[n];
+    }
+}
