15b00f5d09b6054630846436e753eb5c9317ed0b
金初阳 <2468231855@qq.com>
2018-01-15 11:20:19 +0800

    746

---
+++ src/Q746/main.java
@@ -0,0 +1,17 @@
+package Q746;
+
+/**
+ * Created by jinchuyang on 2018/1/15.
+ */
+public class main {
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost.length <= 2){return 0;}
+        int pay[] = new int[cost.length + 1];
+        pay[0] = 0;
+        pay[1] = 0;
+        for(int i = 2; i < pay.length; i++){
+            pay[i] = Math.min(pay[i - 1] + cost[i - 1], pay[i - 2] + cost[i - 2]);
+        }
+        return pay[cost.length];
+    }
+}
