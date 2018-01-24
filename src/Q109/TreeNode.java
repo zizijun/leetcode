f8f132d0d15c2614879242b603582883220d1bb7
金初阳 <2468231855@qq.com>
2018-01-24 22:03:19 +0800

    95

--- src/Q109/TreeNode.java
+++ src/Q109/TreeNode.java
@@ -4,10 +4,10 @@ package Q109;
  * Created by jinchuyang on 2017/12/2.
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int val){
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val){
         this.val = val;
     }
 }
