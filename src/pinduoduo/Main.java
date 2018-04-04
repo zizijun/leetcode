f5f17f0a2be6e56065b1a465581f1d4b3dd63977
金初阳 <2468231855@qq.com>
2018-04-04 09:40:34 +0800

    拼多多笔试

--- src/pinduoduo/Main.java
+++ src/pinduoduo/Main.java
@@ -1,7 +1,29 @@
 package pinduoduo;
 
+import java.util.Scanner;
+
 /**
  * Created by jinchuyang on 2018/4/3.
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        int hour = Integer.parseInt(input.split(":")[0]);
+        int minute = Integer.parseInt(input.split(":")[1]);
+        hour = hour % 12;
+        if(minute % 2 == 0){
+            int degree1 = (int)((hour + minute / 60.0) * 30);
+            int degree2 = minute * 6;
+            int diff = Math.abs(degree1 - degree2) > 180 ? 360 - Math.abs(degree1 - degree2): Math.abs(degree1 - degree2);
+            System.out.println(diff);
+        }else {
+            double degree1 = (hour + minute / 60.0) * 30;
+            double degree2 = minute * 6;
+            double diff = Math.abs(degree1 - degree2) > 180 ? 360 - Math.abs(degree1 - degree2): Math.abs(degree1 - degree2);
+            System.out.println((int)(diff * 10) / 10.0);
+        }
+
+
+    }
 }
--- src/pinduoduo/Main4.java
+++ src/pinduoduo/Main4.java
@@ -1,7 +1,104 @@
 package pinduoduo;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by jinchuyang on 2018/4/3.
  */
 public class Main4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int nums[] = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = in.nextInt();
+        }
+
+        int sortNums[]  = new int[k];
+        int[][] map = new int[n][2];
+        for (int i = 0; i < k; i++) {
+            sortNums[i] = nums[i];
+
+        }
+
+
+
+
+
+    }
+    private static class BigHeap{
+        int index;
+        int[] num ;
+
+        public BigHeap(int[] num) {
+            this.num = num;
+        }
+
+        public void heapSort(){
+            int n = num.length;
+            for(int i = n/2;i>=0;i--){   //建堆，从n/2开始向上调整
+                adjustHeap(i, n);
+            }
+            for(int j=n-1;j>=0;j--){
+                swap(0, j);         //交换开始和最后位置上的数字
+                adjustHeap(0, j);    //交换之后再调整堆
+            }
+        }
+        public void adjustHeap(int s,int n){  //由于根节点编号是从0开始的，所以其左子树是2*i+1
+            for(int i=s;i<n;){
+                int max = i;
+                if((2*i+1)<n&&num[2*i+1]>num[max]) max = 2*i+1;
+                if((2*i+2)<n&&num[2*i+2]>num[max]) max = 2*i+2;
+                if(max!=i){
+                    swap(i,max);
+                    i=max;
+                }else break;
+
+            }
+
+        }
+        public void swap(int i,int j){
+            int tem = num[i];
+            num[i] = num[j];
+            num[j] = tem;
+        }
+    }
+    private static class SmallHeap{
+        int[] num ;
+
+        public SmallHeap(int[] num) {
+            this.num = num;
+        }
+
+        public void heapSort(){
+            int n = num.length;
+            for(int i = n/2;i>=0;i--){   //建堆，从n/2开始向上调整
+                adjustHeap(i, n);
+            }
+            for(int j=n-1;j>=0;j--){
+                swap(0, j);         //交换开始和最后位置上的数字
+                adjustHeap(0, j);    //交换之后再调整堆
+            }
+        }
+        public void adjustHeap(int s,int n){  //由于根节点编号是从i开始的，所以其左子树是2*i+1
+            for(int i=s;i<n;){
+                int min = i;
+                if((2*i+1)<n&&num[2*i+1]<num[min]) min = 2*i+1;
+                if((2*i+2)<n&&num[2*i+2]>num[min]) min = 2*i+2;
+                if(min!=i){
+                    swap(i,min);
+                    i=min;
+                }else break;
+
+            }
+
+        }
+        public void swap(int i,int j){
+            int tem = num[i];
+            num[i] = num[j];
+            num[j] = tem;
+        }
+    }
 }
