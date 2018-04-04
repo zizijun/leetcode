4fbb3da43b0ea6cb70696f7a6d0b4a568a266930
金初阳 <2468231855@qq.com>
2018-04-04 09:40:00 +0800

    serializable

--- src/Serializable/Test.java
+++ src/Serializable/Test.java
@@ -1,7 +1,25 @@
 package Serializable;
 
+import java.io.*;
+
 /**
  * Created by jinchuyang on 2018/4/3.
  */
 public class Test {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+//        User user = new User();
+//        user.setAge("22");
+//        user.setName("aa");
+//        user.setPassword("admin");
+//        System.out.println(user.getAge()+"\t"+user.getName()+"\t"+user.getPassword());
+//        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/Serializable/user.txt"));
+//        oos.writeObject(user);
+//        user.setAge("23");
+//        oos.flush();
+//        oos.close();
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("src/Serializable/user.txt"));
+        User users = (User) ois.readObject();
+        System.out.println(users.getAge()+"\t"+users.getName()+"\t"+users.getPassword());
+    }
 }
--- src/Serializable/User.java
+++ src/Serializable/User.java
@@ -1,7 +1,41 @@
 package Serializable;
 
+import java.io.Serializable;
+
 /**
  * Created by jinchuyang on 2018/4/3.
  */
-public class Student {
+public class User implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private transient String password;   //被transient修饰的变量
+    private static int age;
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static int getAge() {
+        return age;
+    }
+
+    public static void setAge(int age) {
+        User.age = age;
+    }
 }
