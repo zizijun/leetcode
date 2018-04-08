a4fb2f5d06e86ba1158fae70c634c897db3eece6
金初阳 <2468231855@qq.com>
2018-04-08 15:00:13 +0800

    模拟LRU

--- src/Q146/Main.java
+++ src/Q146/Main.java
@@ -4,4 +4,16 @@ package Q146;
  * Created by jinchuyang on 2018/4/8.
  */
 public class Main {
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(1);
+        lruCache.put(2, 1);
+        //lruCache.put(2, 2);
+        lruCache.get(2);
+        //lruCache.put(3,3);
+        lruCache.get(2);
+        lruCache.put(4, 4);
+        lruCache.get(1);
+        lruCache.get(3);
+        lruCache.get(4);
+    }
 }
